639f0a6cbc1b0a84c99ae7b5592d1d84ca40910b
Leena <panchalleena8839@gmail.com>
2025-03-24 23:00:01 +0530

    Initial commit

---
+++ src/primeFactors.java
@@ -0,0 +1,16 @@
+public class primeFactors {
+
+    public static boolean isPrime(int n){
+        
+    }
+    public static void main(String[] args) {
+        int n =24;
+
+        for(int i=1; i<=n; i++){
+
+            if (n%i == 0) {
+                
+            }
+        }
+    }
+}
