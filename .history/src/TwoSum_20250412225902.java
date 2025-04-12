5350872790ef9d3885f0b70345c9670815d822e7
Leena <panchalleena8839@gmail.com>
2025-04-12 23:01:38 +0530

    Example on function

---
+++ .history/src/TwoSum_20250412225902.java
@@ -0,0 +1,11 @@
+public class TwoSum {
+
+    public static int Sum(int a, int b)
+    {
+        int answer = a+b;
+        return answer;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/TwoSum.java
@@ -0,0 +1,12 @@
+public class TwoSum {
+
+    public static int Sum(int a, int b)
+    {
+        int answer = a+b;
+        return answer;
+    }
+    public static void main(String[] args) {
+        int c = Sum(5, 6);
+        System.out.println(c);
+    }
+}
