bd4b3ea74254cc5103f09ef2d1a9cb75a5f3c29b
Leena <panchalleena8839@gmail.com>
2025-04-16 19:45:30 +0530

    creating array

---
+++ .history/src/ArrayExample_20250416194251.java
@@ -0,0 +1,15 @@
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        
+        int arr[] = new int[3];
+        double ar[] = new double[4];
+        float arr1[] = new float[3];
+        char arr2[] = new char[3];
+
+        String s[] = new String[3];
+
+
+    }
+    
+}
---
+++ src/ArrayExample.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        
+        int arr[] = new int[3];
+        double ar[] = new double[4];
+        float arr1[] = new float[3];
+        char arr2[] = new char[3];
+
+        String s[] = new String[3];
+
+        //user input size array
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arrr[] = new int[n];
+
+    }
+    
+}
