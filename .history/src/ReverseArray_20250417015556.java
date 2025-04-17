0289e0d28717018025028fe0838fe080074aca50
Leena <panchalleena8839@gmail.com>
2025-04-18 00:38:35 +0530

    Question practice on Array

---
+++ .history/src/ReverseArray_20250417015556.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" Enter size of an array : ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println(" Enter Elements of an array : ");
+        for (int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int i = 0;//start point
+        int j = n-1;//end point
+
+        while(i < j)
+        {
+
+        }
+
+    }
+}
---
+++ .history/src/smallestAndSecondSmallestElemnetInArray_20250417014435.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class smallestAndSecondSmallestElemnetInArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" Enter size of an array : ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println(" Enter Elements of an array : ");
+        for (int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int minE = (int)(1e9);
+        int secondMinE = (int)(1e9);
+
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] < minE)
+            {
+                secondMinE = minE;
+                minE = arr[i];
+            }
+            else
+            {
+                if(arr[i] > minE && arr[i] < secondMinE)
+                {
+                    secondMinE = arr[i];
+                }
+            }
+        }
+
+        System.out.println(minE);
+        System.out.println(secondMinE);
+    }
+}
