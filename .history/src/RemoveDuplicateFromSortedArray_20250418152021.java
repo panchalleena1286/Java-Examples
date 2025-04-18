d8c7abf47d831d59925ba3e5d48f92557e893a24
Leena <panchalleena8839@gmail.com>
2025-04-18 15:22:05 +0530

    Remove duplicates from array

---
+++ .history/src/RemoveDuplicateFromSortedArray_20250418152021.java
@@ -0,0 +1,26 @@
+public class RemoveDuplicateFromSortedArray {
+
+    public void removeDuplicates(int ar[]){
+
+        int i = 0;
+        
+        for(int j=0; j<ar.length; j++){
+            if (ar[j] != ar[i])
+            {
+                i++;
+                ar[i] =ar[j];
+            }
+        }
+        return i+1;
+    }
+    
+    public static void main(String[] args) {
+
+        int arr[] = {0,0,1,1,1,2,2,3,3,4};
+
+
+
+        
+    }
+    
+}
