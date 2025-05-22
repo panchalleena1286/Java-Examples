b6a2fa508912000ca88448bc743b606a34a99055
Leena <panchalleena8839@gmail.com>
2025-05-22 19:35:44 +0530

    Initial changes

--- .history/src/ExampleOnString4_20250522193449.java
+++ .history/src/ExampleOnString4_20250522193449.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class ExampleOnString3 {
+public class ExampleOnString4 {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
 
         System.out.println(s);
-        System.out.println(s.length());
-        
+
+        for(int i=0; i<s.length(); i++)
+        {
+            System.err.println(s.charAt(i));
+        }
     }
+    
 }
