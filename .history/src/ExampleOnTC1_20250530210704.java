e6c48b2f49913954c827ad9caf77574ddc81dac5
Leena <panchalleena8839@gmail.com>
2025-05-30 21:27:30 +0530

    Example on time complexity

---
+++ .history/src/ExampleOnTC1_20250530210704.java
@@ -0,0 +1,10 @@
+public class ExampleOnTC1 {
+    public static void main(String[] args) {
+        int n =5;
+        int sum = n*(n+1)/2;
+        System.out.println(sum);
+    }
+
+    //TC = o(1)
+    //SC = o(1)
+}
