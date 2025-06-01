a03abcc2f52ab303c8d831b9f37abd38adb17a79
Leena <panchalleena8839@gmail.com>
2025-06-01 23:32:05 +0530

    Example on arraylist

---
+++ .history/src/ExampleOnArrayListOfArrayList_20250601231954.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ExampleOnArrayListOfArrayList {
+    public static void main(String[] args) {
+        
+        ArrayList<ArrayList<Integer>> ar = new ArrayList<>();
+
+        ArrayList<Integer> row = new ArrayList<>();
+
+       row.add(1);
+       row.add(2);
+       row.add(3);
+
+       ar.add(new ArrayList<>(row));//[[1,2,3]]
+
+       System.out.print(ar);
+
+       row.remove(2);
+
+       ar.add(new ArrayList<>(row));
+
+       System.out.println(ar);;
+
+
+
+
+    }
+}
