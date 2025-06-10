0916f353ef3de117df91dd9e8c063d698459764d
Leena <panchalleena8839@gmail.com>
2025-06-10 22:33:40 +0530

    Example on sum of subarray using Fixing techniques

---
+++ .history/src/FixingTechniqueSumOfSubArray_20250610223008.java
@@ -0,0 +1,20 @@
+public class FixingTechniqueSumOfSubArray {
+
+    public static void main(String[] args) {
+        
+        int arr[] = {1, 2, 3};
+        int n = arr.length;
+
+        int sum = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                sum = sum + arr[j];
+                System.out.println("Subarray from " + i + " to " + j + " sum = " + sum);
+            }
+        }
+    }
+    
+}
