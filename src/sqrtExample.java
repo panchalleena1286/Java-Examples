73006531fdca8c683303bf5d49f0c15499360511
Leena <panchalleena8839@gmail.com>
2025-06-20 23:48:57 +0530

    Example on sqrt

---
+++ src/sqrtExample.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class sqrtExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(sqrt(N));
+    }
+
+    public static int sqrt(int N) {
+        int low = 0, high = N, ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            long square = (long) mid * mid; // long to avoid overflow
+
+            if (square == N) {
+                return mid; // perfect square
+            } else if (square < N) {
+                ans = mid;  // possible answer
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
