c83f82e506b8ae9d0043c1918672a472fe5f893a
Leena <panchalleena8839@gmail.com>
2025-06-21 23:45:18 +0530

    Example on Running Sum of Array

---
+++ .history/src/RunnigSumofArray_20250621234502.java
@@ -0,0 +1,17 @@
+public class RunnigSumofArray {
+    
+    public static void main(String[] args) {
+        
+        int[] nums = {1,2,3,4,5};
+
+        int n = nums.length;
+
+        int perfect[] = new int[n];
+
+        
+
+
+
+
+    }
+}
