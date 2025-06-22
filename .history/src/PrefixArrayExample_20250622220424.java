e5f741af010cdfa6a947d2c84afc3317608e67dc
Leena <panchalleena8839@gmail.com>
2025-06-22 22:04:40 +0530

    Example on Prefix Array

---
+++ .history/src/PrefixArrayExample_20250622220424.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PrefixArrayExample {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int diff[] = new int[n+1];
+
+        int q = sc.nextInt();
+
+        for (int i=0; i<q; i++){
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int val = sc.nextInt();
+
+            diff[l] += val;
+            diff[r+1] -= val;
+
+            int sum =0;
+
+            
+        }
+
+    }
+    
+}
