06bff2a73f38f54990c44bbe6938b0a854bc3479
Leena <panchalleena8839@gmail.com>
2025-07-14 20:45:00 +0530

    example on calculator

---
+++ .history/src/calculator_20250702162056.java
@@ -0,0 +1,6 @@
+public class calculator {
+     public static void main(String[] args) {
+        Cal c = (a, b) -> a + b ;
+        System.out.println(cal.add(5, 7));
+     }
+}
---
+++ .history/src/calculator_20250714204448.java
@@ -0,0 +1,12 @@
+public class calculator {
+
+    @FunctionalInterface
+interface Calculator{
+int add(int a , int b);
+}
+
+     public static void main(String[] args) {
+        Cal c = (a, b) -> a + b ;
+        System.out.println(cal.add(5, 7));
+     }
+}
