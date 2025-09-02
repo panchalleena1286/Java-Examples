fab2bdb32139a7123b828504bf58f44682f071d6
Leena <panchalleena8839@gmail.com>
2025-09-02 22:59:36 +0530

    Example on Modulus properties

---
+++ .history/src/ModulusOfProgramming_20250902225850.java
@@ -0,0 +1,9 @@
+public class ModulusOfProgramming {
+
+    public static void main(String[] args) {
+         
+        int N = 100;
+
+        int MOD = 100000000 + 7;
+    }
+}
---
+++ .history/src/ModulusOfProgramming_20250902225921.java
@@ -0,0 +1,10 @@
+public class ModulusOfProgramming {
+
+    public static void main(String[] args) {
+         
+        int N = 100;
+
+        int MOD = 1000000000 + 7;
+        int sum =0;
+    }
+}
