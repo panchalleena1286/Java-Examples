8aa19bccf7d937ba15c01c28d0eca235d4d2c756
Leena <panchalleena8839@gmail.com>
2025-09-04 23:04:23 +0530

    Example fibonacci series using recursion

---
+++ .history/src/FibonacciUsingRecursion_20250904224358.java
@@ -0,0 +1,17 @@
+public class FibonacciUsingRecursion {
+
+    static int fibo(int n){
+
+        if (n==0 || n==1) {
+            return 1;
+        }
+
+        int answer = fibo(n-1) + fibo(n-2);
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println(fibo(10));
+    }
+}
