3a4457970876602570837d324ea2fadd73872905
Leena <panchalleena8839@gmail.com>
2025-09-05 18:58:34 +0530

    Example on index occurence of element

--- .history/src/OccurenceOfElementUsingRecursion_20250905185550.java
+++ .history/src/OccurenceOfElementUsingRecursion_20250905185550.java
@@ -2,22 +2,15 @@ public class OccurenceOfElementUsingRecursion {
 
     public static void occurEle(int index, int arr[], int target){
 
-        if (index >= arr.length) {
-            return;
-        }
 
         if (arr[index] == target) {
-            System.out.print(index);
+            occurEle(index + 1, arr, target);
         }
-
-        occurEle(index + 1, arr, target);
     }
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,6,4,3,5,3,3};
         int target = 3;
-
-        occurEle(0, arr, target);
 
     }
 }
