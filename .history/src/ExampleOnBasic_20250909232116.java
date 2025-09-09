5142a151496ec6b64e6af45737ac12432ff532bd
Leena <panchalleena8839@gmail.com>
2025-09-09 23:23:30 +0530

    Example on prime number

---
+++ .history/src/ExampleOnBasic_20250909232116.java
@@ -0,0 +1,5 @@
+public class ExampleOnBasic {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ .history/src/ExampleOnBasic_20250909232314.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExampleOnBasic {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("enter the Element : ");
+
+    }
+}
