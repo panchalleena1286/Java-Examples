a37ad0b534367a851520bbf31cf7b2412df2df04
Leena <panchalleena8839@gmail.com>
2025-09-12 23:54:17 +0530

    Example on method overriding

--- .history/src/OverridingConcept/Example_20250911225514.java
+++ .history/src/OverridingConcept/Example_20250911225514.java
@@ -2,9 +2,9 @@ package OverridingConcept;
 
 // #Method Oveeriding 
 //Modifying a super class method in the sub class is called method overriding. Using method overriding,
-//we can change super class method according to the requirements of sub class.
+ //we can change super class method according to the requirements of sub class.
 
-//Static Methods - We can not override static methods. 
+ 
 
 public class Example {
     // Case 1: Normal Overriding (Works Fine)
