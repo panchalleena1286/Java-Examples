74bb431128859ca64bca252235ea96905adcd61e
Leena <panchalleena8839@gmail.com>
2026-05-28 22:00:02 +0530

    Stream Map example

---
+++ .history/src/StreamMap_20260528215952.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamMap {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Java", "Stream", "API");
+
+List<Integer> result = words.stream()
+        .map(String::length)
+        .toList();
+
+
+    }
+}
