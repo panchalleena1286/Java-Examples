ffaf58b3bc696b8abd52c5e9703c8aec18df0914
Leena <panchalleena8839@gmail.com>
2026-05-30 21:38:45 +0530

    Example on streaM

---
+++ src/StreamExamp4.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExamp4 {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList(
+            new Emplist("John", 4000),
+            new Emplist("Alie", 6000)
+        );
+    }
+}
